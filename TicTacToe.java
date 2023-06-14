98978732c33ab60abaafa43a566d08feb1023146
Pulkit <kpulkit15234@gmail.com>
2023-06-14 20:07:54 +0530

    Implementing the winning condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -28,7 +28,7 @@ public class TicTacToe {
             if (c=='O'){
                 System.out.println("Turn for X");
             } else{
-                System.out.println("Turn for Y");
+                System.out.println("Turn for O");
             }
             
             System.out.print("Enter row - ");
@@ -60,11 +60,12 @@ public class TicTacToe {
     }
 
     public static void printBoard(char[][] board) {
+        int a = 2;
         System.out.println("  ___________ ");
         System.out.println(" |           |");
-        for (int row = 2; row >= 0; row--) {
-            System.out.print(" | "+row+" | ");
-            for (int col = 2; col >= 0; col--) {
+        for (int row = 0; row < 3; row++) {
+            System.out.print(" | "+a+" | "); a--;
+            for (int col = 0; col < 3; col++) {
                 System.out.print(board[row][col]+" ");
             }
             System.out.println("|");
@@ -75,6 +76,16 @@ public class TicTacToe {
     }
     
     public static boolean haveWon(char[][] board , char c) {
-        return true ;
+        if (
+            ( (board[0][0]!=' ') && (board[0][0] == board[0][1]) && (board[0][1] == board[0][2]) ) || //comapring first row elements
+            ( (board[1][0]!=' ') && (board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) ) || //comapring second row elements
+            ( (board[2][0]!=' ') && (board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) ) || //comapring third row elements
+            ( (board[0][0]!=' ') && (board[0][0] == board[1][0]) && (board[1][0] == board[2][0]) ) || //comapring first column elements
+            ( (board[1][0]!=' ') && (board[1][0] == board[1][1]) && (board[1][1] == board[2][1]) ) || //comapring second column elements
+            ( (board[0][2]!=' ') && (board[0][2] == board[1][2]) && (board[1][2] == board[2][2]) ) || //comapring third column elements
+            ( (board[0][0]!=' ') && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) ) || //comapring column elements
+            ( (board[0][2]!=' ') && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) )  //comapring column elements
+        ) return true;
+        return false;
     }
 }
