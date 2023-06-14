6d66b27c440ba1a0a374803d5905c490bb90aff1
Pulkit <kpulkit15234@gmail.com>
2023-06-14 18:48:47 +0530

    Implement the codition for empty multiple entries

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,57 @@
 import java.util.Scanner;
 
 public class TicTacToe {
+    
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        //Initializing the empty board
+        for (int row = 2; row >= 0; row--) {
+            for (int column = 2; column >= 0; column--) {
+                board[row][column] = ' ';
+            }
+        }
+        
+        //Scanner Object
+        Scanner sc = new Scanner(System.in);
+        
+        //flag variables
+        boolean gameOver = false;
+        char c = 'O';
+        int row,col,count=0;
+        
+        
+        while (!gameOver && count <9){
+
+            printBoard(board);
+            
+            if (c=='O'){
+                System.out.println("Turn for X");
+            } else{
+                System.out.println("Turn for Y");
+            }
+            
+            System.out.print("Enter row - ");
+            row = sc.nextInt();
+            System.out.print("Enter column - ");
+            col = sc.nextInt();
+            if (board[row][col] == ' '){
+                count++;
+                
+            } else{
+                System.out.println("\nInvalid move!");
+                continue;
+            }
+            
+            
+            board[row][col] = c;
+            
+            
+            c= (c=='X') ? 'Y':'X';
+            
+        }   
+    }
+
     public static void printBoard(char[][] board) {
         System.out.println("  ___________ ");
         System.out.println(" |           |");
@@ -15,49 +66,5 @@ public class TicTacToe {
         System.out.println(" | 0 | 0 1 2 |");
         System.out.println(" |___|_______|");
     }
-
-    public static void main(String[] args) {
-        char[][] board = new char[3][3];
-
-        //Initializing the empty board
-        for (int row = 2; row >= 0; row--) {
-            for (int column = 2; column >= 0; column--) {
-                board[row][column] = ' ';
-            }
-        }
-        
-        //Scanner Object
-        Scanner sc = new Scanner(System.in);
-
-        //flag variables
-        boolean gameOver = false;
-        char c = 'O';
-        int row,col,count=0;
-
-
-        while (!gameOver && count <9){
-            printBoard(board);
-            if (c=='O'){
-                 System.out.println("Turn for X");
-            } else{
-                System.out.println("Turn for Y");
-            }
-            System.out.print("Enter row - ");
-            row = sc.nextInt();
-            System.out.print("Enter column - ");
-            col = sc.nextInt();
-            if (board[row][col] == ' '){
-                count++;
-
-            } else{
-                System.out.println("\nInvalid move");
-                continue;
-            }
-
-
-            board[row][col] = c;
-            c= (c=='X') ? 'Y':'X';
-
-        }   
-    }
+    
 }
