3c8d5c403bfadffb160f421617cfb9fa6b40edb6
Pulkit <kpulkit15234@gmail.com>
2023-06-14 18:24:23 +0530

    printBoard() + printing an empty board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,28 @@
-public class TicTacToe{
+public class TicTacToe {
+    public static void printBoard(char[][] board) {
+        System.out.println("  ___________ ");
+        System.out.println(" |           |");
+        for (int row = 2; row >= 0; row--) {
+            System.out.print(" | "+row+" | ");
+            for (int column = 2; column >= 0; column--) {
+                System.out.print(board[row][0]+" ");
+            }
+            System.out.println("|");
+        }
+        System.out.println(" | --|------ |");
+        System.out.println(" | 0 | 0 1 2 |");
+        System.out.println(" |___|_______|");
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is a Tic Tac Toe game");
+        char[][] board = new char[3][3];
+
+        for (int row = 2; row >= 0; row--) {
+            for (int column = 2; column >= 0; column--) {
+                board[row][column] = ' ';
+            }
+        }
+
+        printBoard(board);
     }
 }
