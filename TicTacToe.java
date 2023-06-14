92a4833e25b98bf75228d323ff824fb9f434fac7
Pulkit <kpulkit15234@gmail.com>
2023-06-14 18:36:41 +0530

    Stimulating simple input of O/X and displaying using while loop

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void printBoard(char[][] board) {
         System.out.println("  ___________ ");
         System.out.println(" |           |");
         for (int row = 2; row >= 0; row--) {
             System.out.print(" | "+row+" | ");
-            for (int column = 2; column >= 0; column--) {
-                System.out.print(board[row][0]+" ");
+            for (int col = 2; col >= 0; col--) {
+                System.out.print(board[row][col]+" ");
             }
             System.out.println("|");
         }
@@ -17,12 +19,44 @@ public class TicTacToe {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
 
+        //Initializing the empty board
         for (int row = 2; row >= 0; row--) {
             for (int column = 2; column >= 0; column--) {
                 board[row][column] = ' ';
             }
         }
+        
+        //Scanner Object
+        Scanner sc = new Scanner(System.in);
 
-        printBoard(board);
+        //flag variables
+        boolean gameOver = false;
+        char c = 'O';
+        int row,col,count=0;
+
+
+        while (!gameOver && count <9){
+            printBoard(board);
+            if (c=='O'){
+                System.out.println("Turn for X");
+                System.out.print("Enter row - ");
+                row = sc.nextInt();
+                System.out.print("Enter column - ");
+                col = sc.nextInt();
+
+                board[row][col] = 'O';
+                c = 'X';
+            } else{
+                System.out.println("Turn for O");
+                System.out.print("Enter row - ");
+                row = sc.nextInt();
+                System.out.print("Enter column - ");
+                col = sc.nextInt();
+
+                board[row][col] = 'X';
+                c = 'O';
+            }
+            count++;
+        }
     }
 }
